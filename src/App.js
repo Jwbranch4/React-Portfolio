e7792e8ea59c209ba0da39543d4422e917e42042
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2022-01-20 01:38:34 -0600

    changed to hashrouter for githubpages

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import Header from "./components/Header";
 import Project from "./components/Project";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <Router basename={"https://jwbranch4.github.io/React-Portfolio/"}>
-      <div>
-        <Header> </Header>
-        <main>
-          <Route exact path="/" component={About} />
-          <Route exact path="/projects" component={Project} />
-          <Route exact path="/contact" component={Contact} />
-        </main>
-        <Footer></Footer>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename={"/React-Portfolio/"}>
+        <div>
+          <Header> </Header>
+          <main>
+            <Switch>
+              <Route path="/" component={About} />
+              <Route path="/projects" component={Project} />
+              <Route path="/contact" component={Contact} />
+            </Switch>
+          </main>
+          <Footer></Footer>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
