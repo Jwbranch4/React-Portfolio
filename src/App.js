3f78246348ae4f346361fb455071c6fd4b097b6b
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2022-01-20 01:53:24 -0600

    trying to deploy to heroku instead of github pages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,22 @@ import Header from "./components/Header";
 import Project from "./components/Project";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import React, { Component } from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter basename={"/React-Portfolio/"}>
-        <div>
-          <Header> </Header>
-          <main>
-            <Switch>
-              <Route path="/" component={About} />
-              <Route path="/projects" component={Project} />
-              <Route path="/contact" component={Contact} />
-            </Switch>
-          </main>
-          <Footer></Footer>
-        </div>
-      </HashRouter>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header> </Header>
+        <main>
+          <Route exact path="/" component={About} />
+          <Route exact path="/projects" component={Project} />
+          <Route exact path="/contact" component={Contact} />
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
